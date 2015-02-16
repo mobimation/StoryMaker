3cbcc48cd482e6a329025be1e46a20dacb6e92a2
mobimation <gunnar.forsgren@mobimation.com>
2015-02-16 01:27:28 +0100

    Initial definition of class structure.

--- app/src/main/java/com/mobimation/storymaker/Clip.java
+++ app/src/main/java/com/mobimation/storymaker/Clip.java
@@ -3,7 +3,8 @@ package com.mobimation.storymaker;
 /**
  * Created by gunnarforsgren on 15-02-16.
  *
- * A Clip is a piece of an Artifact.
+ * A Clip is a piece of an Artifact copied from a start position.
+ * and with a duration. A Clip is stored as a separate data entity copy.
  *
  */
 public class Clip {
